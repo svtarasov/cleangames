530e5936024d7ef0c7cc6ceaed0bde5ec7a3a1cd
malchkate <katerina.malch@gmail.com>
2015-07-09 11:57:25 +0300

    Added button for menu

--- MapsEx/app/src/main/java/com/example/katerina/mapsex/TeamsActivity.java
+++ MapsEx/app/src/main/java/com/example/katerina/mapsex/TeamsActivity.java
@@ -4,10 +4,12 @@ import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -49,6 +51,7 @@ public class TeamsActivity extends ActionBarActivity {
             }
         });
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
