886833a9219d062db18bcc74265851b7479b3bcf
malchkate <katerina.malch@gmail.com>
2015-07-12 20:50:26 +0300

    Added button Menu in Teams View.

--- MapsEx/app/src/main/java/com/example/katerina/mapsex/TeamsActivity.java
+++ MapsEx/app/src/main/java/com/example/katerina/mapsex/TeamsActivity.java
@@ -11,11 +11,12 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 
-public class TeamsActivity extends ActionBarActivity {
+public class TeamsActivity extends ActionBarActivity implements PopupMenu.OnMenuItemClickListener {
 
     public TeamsAdapter mAdapter;
 
@@ -24,21 +25,21 @@ public class TeamsActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teams);
         Bundle extras = getIntent().getExtras();
-        String str ;
-        if(extras == null) {
-            str= null;
+        String str;
+        if (extras == null) {
+            str = null;
         } else {
-            str= extras.getString("Name");
+            str = extras.getString("Name");
         }
         setTitle("Teams in game : " + str);
 
         final ListView listViewTeams = (ListView) findViewById(R.id.listTeams);
-       // listViewTeams.getSelectedItem()
+        // listViewTeams.getSelectedItem()
         ArrayList<Team> exampleList = new ArrayList<Team>();
         exampleList.add(new Team("1", "something"));
         exampleList.add(new Team("2", "anything"));
         exampleList.add(new Team("3", "nothing"));
-        mAdapter = new TeamsAdapter(this,exampleList);
+        mAdapter = new TeamsAdapter(this, exampleList);
         listViewTeams.setAdapter(mAdapter);
         listViewTeams.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -56,7 +57,47 @@ public class TeamsActivity extends ActionBarActivity {
                 startActivity(new Intent(TeamsActivity.this, NewTeamActivity.class));
             }
         });
+
+        findViewById(R.id.button_popup)
+                //Следим за нажатиями по кнопке:
+                .setOnClickListener(new View.OnClickListener() {
+
+                    //Обрабатываем нажатие кнопки Button:
+                    @Override
+                    public void onClick(View view) {
+                        //Вызываем popup меню, заполняем его с файла popup.xml и настраиваем
+                        //слушатель нажатий по пунктам OnMenuItemClickListener:
+                        PopupMenu popup_menu = new PopupMenu(TeamsActivity.this, view);
+                        popup_menu.setOnMenuItemClickListener(TeamsActivity.this);
+                        popup_menu.inflate(R.menu.popup_menu);
+                        popup_menu.show();
+                    }
+                });
     }
+
+    //Обрабатываем нажатия по пунктам popup меню, ссылаясь на id каждого пункта, заданные в файле popup.xml:
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.game_menu:
+                //Toast.makeText(this, "Выбран пункт 1", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(TeamsActivity.this, GamesActivity.class));
+                return true;
+            case R.id.teams_menu:
+                //Toast.makeText(this, "Выбран пункт 2", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(TeamsActivity.this, TeamsActivity.class));
+                return true;
+            case R.id.map_menu:
+                //Toast.makeText(this, "Выбран пункт 3", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(TeamsActivity.this, DemoActivity.class));
+                return true;
+            case R.id.rating_menu:
+                Toast.makeText(this, "You chose Rating", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return true;
+    }
+
+
 
 
     @Override
