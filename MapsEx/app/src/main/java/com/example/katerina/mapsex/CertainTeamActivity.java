95975f87dac7a8379a6b9b770a324afb377db1f1
malchkate <katerina.malch@gmail.com>
2015-07-15 13:46:01 +0300

    Added all menus.

--- MapsEx/app/src/main/java/com/example/katerina/mapsex/CertainTeamActivity.java
+++ MapsEx/app/src/main/java/com/example/katerina/mapsex/CertainTeamActivity.java
@@ -7,6 +7,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.PopupMenu;
 
 import com.example.katerina.mapsex.datamodels.Team;
 import com.example.katerina.mapsex.datamodels.User;
@@ -38,6 +39,42 @@ public class CertainTeamActivity extends ActionBarActivity {
         ArrayList<User> exampleList = Repository.getUsers(new Team());
         mAdapter = new CertainTeamAdapter(this,exampleList);
         listViewUsers.setAdapter(mAdapter);
+        findViewById(R.id.button_popup)
+                //Следим за нажатиями по кнопке:
+                .setOnClickListener(new View.OnClickListener() {
+
+                    //Обрабатываем нажатие кнопки Button:
+                    @Override
+                    public void onClick(View view) {
+                        //Вызываем popup меню, заполняем его с файла popup.xml и настраиваем
+                        //слушатель нажатий по пунктам OnMenuItemClickListener:
+                        PopupMenu popup_menu = new PopupMenu(CertainTeamActivity.this, view);
+                        popup_menu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                            public boolean onMenuItemClick(MenuItem item) {
+                                switch (item.getItemId()) {
+                                    case R.id.game_menu:
+                                        //Toast.makeText(this, "Выбран пункт 1", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(CertainTeamActivity.this, GamesActivity.class));
+                                        return true;
+                                    case R.id.teams_menu:
+                                        //Toast.makeText(this, "Выбран пункт 2", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(CertainTeamActivity.this, TeamsActivity.class));
+                                        return true;
+                                    case R.id.map_menu:
+                                        //Toast.makeText(this, "Выбран пункт 3", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(CertainTeamActivity.this, DemoActivity.class));
+                                        return true;
+                                    case R.id.rating_menu:
+                                        startActivity(new Intent(CertainTeamActivity.this, RatingActivity.class));
+                                        return true;
+                                }
+                                return true;
+                            }
+                        });
+                        popup_menu.inflate(R.menu.popup_menu);
+                        popup_menu.show();
+                    }
+                });
     }
 
     @Override
--- MapsEx/app/src/main/java/com/example/katerina/mapsex/RatingActivity.java
+++ MapsEx/app/src/main/java/com/example/katerina/mapsex/RatingActivity.java
@@ -1,10 +1,13 @@
 package com.example.katerina.mapsex;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.PopupMenu;
 
 import com.example.katerina.mapsex.datamodels.Game;
 import com.example.katerina.mapsex.datamodels.Team;
@@ -24,7 +27,66 @@ public class RatingActivity extends ActionBarActivity {
         ArrayList<Team> exampleList = Repository.getRating(new Game());
         mAdapter = new RatingAdapter(this, exampleList);
         listViewRating.setAdapter(mAdapter);
+        findViewById(R.id.button_popup)
+                //Следим за нажатиями по кнопке:
+                .setOnClickListener(new View.OnClickListener() {
+
+                    //Обрабатываем нажатие кнопки Button:
+                    @Override
+                    public void onClick(View view) {
+                        //Вызываем popup меню, заполняем его с файла popup.xml и настраиваем
+                        //слушатель нажатий по пунктам OnMenuItemClickListener:
+                        PopupMenu popup_menu = new PopupMenu(RatingActivity.this, view);
+                        popup_menu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                            public boolean onMenuItemClick(MenuItem item) {
+                                switch (item.getItemId()) {
+                                    case R.id.game_menu:
+                                        //Toast.makeText(this, "Выбран пункт 1", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(RatingActivity.this, GamesActivity.class));
+                                        return true;
+                                    case R.id.teams_menu:
+                                        //Toast.makeText(this, "Выбран пункт 2", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(RatingActivity.this, TeamsActivity.class));
+                                        return true;
+                                    case R.id.map_menu:
+                                        //Toast.makeText(this, "Выбран пункт 3", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(RatingActivity.this, DemoActivity.class));
+                                        return true;
+                                    case R.id.rating_menu:
+                                        startActivity(new Intent(RatingActivity.this, RatingActivity.class));
+                                        return true;
+                                }
+                                return true;
+                            }
+                        });
+                        popup_menu.inflate(R.menu.popup_menu);
+                        popup_menu.show();
+                    }
+                });
     }
+
+    //Обрабатываем нажатия по пунктам popup меню, ссылаясь на id каждого пункта, заданные в файле popup.xml:
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.game_menu:
+                //Toast.makeText(this, "Выбран пункт 1", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(RatingActivity.this, GamesActivity.class));
+                return true;
+            case R.id.teams_menu:
+                //Toast.makeText(this, "Выбран пункт 2", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(RatingActivity.this, TeamsActivity.class));
+                return true;
+            case R.id.map_menu:
+                //Toast.makeText(this, "Выбран пункт 3", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(RatingActivity.this, DemoActivity.class));
+                return true;
+            case R.id.rating_menu:
+                startActivity(new Intent(RatingActivity.this, RatingActivity.class));
+                return true;
+        }
+        return true;
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
