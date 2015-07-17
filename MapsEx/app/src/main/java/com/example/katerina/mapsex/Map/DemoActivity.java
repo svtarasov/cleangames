8a62a4cea75184aab096b8f37c757c8d3c86e931
malchkate <katerina.malch@gmail.com>
2015-07-17 14:13:37 +0300

    Added reload button.

--- MapsEx/app/src/main/java/com/example/katerina/mapsex/Map/DemoActivity.java
+++ MapsEx/app/src/main/java/com/example/katerina/mapsex/Map/DemoActivity.java
@@ -1,7 +1,9 @@
 package com.example.katerina.mapsex.Map;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.katerina.mapsex.Map.MapFragment;
 import com.example.katerina.mapsex.R;
@@ -19,5 +21,9 @@ public class DemoActivity extends Activity {
                     .add(R.id.container_map_fragment, new MapFragment())
                     .commit();
         }
+    }
+
+    public  void reload_Click(View view){
+        startActivity(new Intent(DemoActivity.this, DemoActivity.class));
     }
 }
