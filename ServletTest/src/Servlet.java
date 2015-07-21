c5416138ca4438005778e297f89be659e4eb40d4
didenko-ilia <il2did@mail.ru>
2015-07-21 16:23:47 +0300

    Updated Servlet
    CreateTeam method available at actionType = CreateTeam, with parameters
    TeamName and UserID

--- ServletTest/src/Servlet.java
+++ ServletTest/src/Servlet.java
@@ -1,64 +1,147 @@
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.Charset;
 import java.sql.*;
 import java.util.Calendar;
+import java.util.Date;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-public class Servlet extends HttpServlet{
+public class Servlet extends HttpServlet {
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws IOException{
+    final String myDriver = "com.mysql.jdbc.Driver";
+    final String myUrl = "jdbc:mysql://mysql48.1gb.ru/gb_cleangames?useUnicode=true&characterEncoding=utf8";
 
-        Connection conn = null;
-        Statement st = null;
-        ResultSet rs = null;
+    final String User = "gb_cleangames";
+    final String Pass = "a5a23237psg";
 
-        final String myDriver = "com.mysql.jdbc.Driver";
-        final String myUrl = "jdbc:mysql://mysql48.1gb.ru/gb_cleangames?useUnicode=true&characterEncoding=utf8";
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
 
-        final String User = "gb_cleangames";
-        final String Pass = "a5a23237psg";
+        request.setCharacterEncoding("UTF-8");
+        response.setHeader("Content-Type", "text/plain; charset=UTF-8");
 
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            conn = DriverManager.getConnection(myUrl, User, Pass);
+        String actionType = request.getParameter("ActionType");
 
-            String query = "insert into Team (ID, Name, CreatedTime)" + " values (?, ?, ?)";
+        if (actionType.equals("CreateTeam"))
+        {
+            String teamName = request.getParameter("TeamName");
+            String userID = request.getParameter("UserID");
+            String str = CreateTeam(teamName,userID);
 
-            Calendar calendar = Calendar.getInstance();
-            java.sql.Date startDate = new java.sql.Date(calendar.getTime().getTime());
+            PrintWriter out = response.getWriter();
+            out.println(str);
 
-            PreparedStatement pS = conn.prepareStatement(query);
-            pS.setInt(1, 94);
-            pS.setString(2, "onlieureurhuerse");
-            pS.setDate(3, startDate);
-
-            pS.execute();
-
-            conn.close();
         }
-        catch (SQLException fd){
-            fd.printStackTrace();
-        }
-        catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        finally {
-            try { if (rs != null) rs.close(); } catch (SQLException e) { e.printStackTrace(); }
-            try { if (st != null) st.close(); } catch (SQLException e) { e.printStackTrace(); }
-            try { if (conn != null) conn.close(); } catch (SQLException e) { e.printStackTrace(); }
-        }
+        else
 
-
-        PrintWriter out = response.getWriter();
-        out.println("<html>");
-        out.println("<body>");
-        out.println("<h1>Team added</h1>");
-        out.println("</body>");
-        out.println("</html>");
+        {
+            PrintWriter out = response.getWriter();
+            out.println("Error");
+        }
     }
+
+    public void Login() {}
+
+    public void Register() {}
+
+    public String CreateTeam(String teamName, String userID) {
+        Connection connection = null;
+        Statement statement = null;
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+
+            connection = DriverManager.getConnection(myUrl, User, Pass);
+
+            String query = "insert into Team (Name, CreatedTime)" + " values (?, ?)";
+
+            long timeNow = Calendar.getInstance().getTimeInMillis();
+            java.sql.Timestamp startDate = new java.sql.Timestamp(timeNow);
+
+            PreparedStatement preparedStatement = connection.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS);
+            preparedStatement.setString(1, teamName);
+            preparedStatement.setTimestamp(2, startDate);
+
+            preparedStatement.executeUpdate();
+
+            ResultSet rs = preparedStatement.getGeneratedKeys();
+            int teamID=0;
+            if (rs.next())
+            {
+                teamID = rs.getInt(1);
+            }
+            query = "UPDATE User SET TeamID = ? WHERE ID = ?";
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, teamID);
+            preparedStatement.setInt(2, new Integer(userID));
+            preparedStatement.execute();
+
+            return "Team added: " + teamName;
+
+        }catch(SQLException se){
+            se.printStackTrace();
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            try{
+                if(statement!=null)
+                    connection.close();
+            }catch(SQLException se){
+            }
+            try{
+                if(connection!=null)
+                    connection.close();
+            }catch(SQLException se) {
+                se.printStackTrace();
+            }
+        }
+        return "Error";
+    }
+
+    public void JoinTeam() {}
+
+    public void LeaveTeam() {}
+
+    public void DeleteTeam() {}
+
+    public void GetTeam() {}
+
+    public void GetTeamList() {}
+
+    public void UpdateTeam() {}
+
+    public void GetProjectList() {}
+
+    public void CreateParameter() {}
+
+    public void DeleteParameter() {}
+
+    public void UpdateParameter() {}
+
+    public void GetParameterList() {}
+
+    public void CreateCheckin() {}
+
+    public void DeleteCheckin() {}
+
+    public void UpdateCheckin() {}
+
+    public void GetCheckinList() {}
+
+    public void GetUserCheckinList() {}
+
+    public void CreateTransfer() {}
+
+    public void UpdateTransfer() {}
+
+    public void DeleteTransfer() {}
+
+    public void GetTransferList() {}
+
+    public void GetLocationList() {}
+
 }
